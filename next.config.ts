247624f32e46166e55ffac140818e826a033e026
rado3336 <rado3336@gmail.com>
2025-03-11 19:52:14 +0100

    feat(create): update pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ipfs.io",
+        port: "",
+        pathname: "/ipfs/**",
+      },
+    ],
+  },
 };
-
-export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,104 @@
+'use client'
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [balance, setBalance] = useState(null);
+  const [adressData, setAdressdata] = useState<any[]>(null);
+  const [nfts, setNfts] = useState<any[]>([]);
+  const [transactions, setTransactions] = useState([]);
+  const [delegation, setDelegation] = useState(null);
+
+  const headers = {
+    project_id: 'mainnetRUrPjKhpsagz4aKOCbvfTPHsF0SmwhLc',
+  };
+
+  const address = 'addr1x88ttk0fk6ssan4g2uf2xtx3anppy3djftmkg959tufsc6qkqt76lg22kjjmnns37fmyue765qz347sxfnyks27ysqaqd3ph23';
+  
+  const fetchData = async () => {
+    try {
+      // Získanie informácií o adrese
+      const addressResponse = await fetch(`https://cardano-mainnet.blockfrost.io/api/v0/addresses/${address}`, { headers });
+      const addressData = await addressResponse.json();
+
+      setAdressdata(addressData)
+      
+      // Výpočet zostatku v ADA
+      const lovelace = addressData.amount.find((amt) => amt.unit === 'lovelace')?.quantity || 0;
+      setBalance(lovelace / 1_000_000);
+
+      // Filtrovanie NFT aktív
+      const nftUnits = addressData.amount.filter((amt) => amt.unit !== 'lovelace');
+      const nftDetails = await Promise.all(
+        nftUnits.map(async (nft) => {
+          const assetResponse = await fetch(`https://cardano-mainnet.blockfrost.io/api/v0/assets/${nft.unit}`, { headers });
+          return assetResponse.json();
+        })
+      );
+
+      setNfts(nftDetails)
+
+    } catch (error) {
+      console.error('Fetch error:', error); 
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="w-full max-w-fit m-10 ">
+        <div className="flex items-center w-full gap-4">
+          <p className="text-2xl font-bold">Zůstatek:</p>
+          <p className="text-xl">{balance} ₳</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <Table className="w-full max-w-fit">
+          <TableHeader className="">
+            <TableRow>
+              <TableHead className="w-full">Název</TableHead>
+              <TableHead className="w-full">název aktiva</TableHead>
+              <TableHead className="w-full">Počet</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className="">
+          {nfts.map((data, index)=>{
+                  const imageUrl = typeof data.onchain_metadata.image === 'string'
+                  ? data.onchain_metadata.image.replace("ipfs://", "https://ipfs.io/ipfs/")
+                  : null;
+  
+                console.log(data.onchain_metadata.name, "data.onchain_metadata.name");
+                console.log(data.onchain_metadata.image, "data.onchain_metadata.image");
+              // const imageUrl = `https://ipfs.io/ipfs/QmV3HtjKZUGeAZnEeytLC3WJaz5txoWzKXhmT7mf2Cinuw`;
+              return(
+              <TableRow key={index}>
+                <TableCell className="text-left w-full">{data.onchain_metadata.name}</TableCell> 
+                <TableCell className="text-left w-full">{data.onchain_metadata.name}<p className="max-w-[4OOpx]"></p></TableCell>
+                <TableCell className="text-left w-full">{data.quantity}</TableCell>
+                <TableCell className="text-left">
+                  {imageUrl ? (
+                    <Image className="w-[50px] h-auto max-w-fit" src={imageUrl} alt={data.onchain_metadata.name} width={50} height={50} />
+                  ) : (
+                    "-"
+                  )}
+                </TableCell>
+          
+              </TableRow>)
+            })}
+          </TableBody>
+        </Table>
     </div>
   );
 }
